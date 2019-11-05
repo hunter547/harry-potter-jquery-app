c9dbdd1d14ca4e2c19f278a795126bfe170b4d2e
Hunter Evanoff <hunter547@gmail.com>
2019-11-04 19:35:30 -0500

    Added bootstrap modal, ul, and button components and screen reader only divs
    Added in the bootstrap UI stylesheet and javascript CDNs. Refactored all buttons, modals, and unordered list elements to bootstrap components. Added support/consideration for screen readers with the sr-only class from bootstrap.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,9 @@ var potterCharacterRepository = (function () {
 					imageUrl: character.image,
 					species: character.species, 
 					wand: character.wand,  
-					patronus: character.patronus
+					patronus: character.patronus, 
+					house: character.house, 
+					gender: character.gender
 				};
 				add(potterCharacter);
 			});
@@ -31,10 +33,10 @@ var potterCharacterRepository = (function () {
 	} 
 
 	// Display all potterCharacter in the repository
-	function addListItem(potterCharacter) {
-		var potterCharacterListItem = $('<li id="'+potterCharacter.name.toLowerCase().replace(/\s/g,'')+'"></li>'); 
-		var potterCharacterLIButton = $('<button class="potter-character-buttons">'
-										+potterCharacter.name+'</button>');
+	function addListItem(potterCharacter) { 
+		var potterCharacterListItem = $('<li class="list-group-item" id="'+potterCharacter.name.toLowerCase().replace(/\s/g,'')+'"></li>'); 
+		var potterCharacterLIButton = $('<button class="btn btn-primary"'+' data-toggle="modal" data-target="#'+potterCharacter.name.toLowerCase().replace(/\s/g,'')+'1'+'">'
+										+potterCharacter.name+'</button>'); 
 		$(potterCharacterListItem).append(potterCharacterLIButton); 
 		$(potterCharacterList).append(potterCharacterListItem); 
 		addPotterCharacterListener(potterCharacterLIButton, potterCharacter);
@@ -42,38 +44,45 @@ var potterCharacterRepository = (function () {
 
 	// Creates event listeners for the buttons as they are being created.
 	function addPotterCharacterListener (button, potterCharacter) {
-		$(button).on('click', function() {
-			showModal(potterCharacter); 
+		$(button).on('click', function() { 
+			showModal(potterCharacter);
 			moveListLeft();
 		});
 	} 
 
 	// MODALS
 
-	function showModal(potterCharacter) {
-		var $modalContainer = $('#modal-container')[0]; 
-		// Clear any existing text if being used again. 
-		$($modalContainer).empty(); 
-
-		// Will close the modal container if the user clicks out side of the modal div 
-		$($modalContainer).on('click', (e) => {
-		 	var target = e.target;
-		  	if (target === $modalContainer) {
-		  		hideModal();
-			}
-		}); 
-
+	function showModal(potterCharacter) {		
 		// Create a modal div to go into the container 
-		var modal = $('<div class="modal"></div>');
+		var modal = $('<div class="modal fade" id="'+potterCharacter.name.toLowerCase().replace(/\s/g,'')+'1'
+			         +'" tabindex="-1" role="dialog" aria-labelledby="'+potterCharacter.name+'" aria-hidden="true"></div>'); 
+		// Will close the modal container if the user clicks out side of the modal div
+		modal.on('click',(e) => {
+			var target = e.target; 
+			if (target.id === modal[0].id) {
+				moveListRight();
+			}
+		});
+		var modalDialog = $('<div class="modal-dialog" role="document"></div>');  
 
-		// Give the modal a close button and title  
-		var closeButton = $('<button class="modal-close">Close</button>');
-		$(closeButton).on('click', hideModal);
+		var modalContent = $('<div class="modal-content"></div>'); 
+		var modalHeader = $('<div class="modal-header"></div>');
+		$(modal).append(modalDialog); 
+		$(modalDialog).append(modalContent);
+		$(modalContent).append(modalHeader); 
 
-		var modalTitle = $('<h1>'+potterCharacter.name+'</h1>'); 
+
+		var modalTitle = $('<h5 class="modal-title" id="'+potterCharacter.name+'">'+potterCharacter.name+'</h5>'); 
+		var modalClose = $('<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+						  +'<span aria-hidden="true">&times;</span></button>'); 
+		modalClose.on('click',moveListRight);
+
+		$(modalHeader).append(modalTitle); 
+		$(modalHeader).append(modalClose);
 
 		// Main modal content div that all other divs will go into
-		var modalBodyDiv = $('<div class="modal-body-div"></div>'); 
+		var modalBodyDiv = $('<div class="modal-body"></div>'); 
+
 
  		// Create a div that the species and patronus data will go into
 		var modalSpeciesPatronusTablesDiv = $('<div class="modal-body-text-div"></div>'); 
@@ -101,9 +110,28 @@ var potterCharacterRepository = (function () {
 		$(patronusCell).text(potterCharacter.patronus.charAt(0).toUpperCase() + potterCharacter.patronus.slice(1)); 
 		if (potterCharacter.patronus === '') {
 			$(patronusCell).text('Unknown');
+		} 
+
+		// Screen Reader consideration for species and patronus information
+		var modalSpeciesPatronusTablesSr = '<div class="sr-only">'+potterCharacter.name+' is a '+potterCharacter.species+' and '; 
+
+		if (potterCharacter.gender==='female'){ 
+			modalSpeciesPatronusTablesSr += 'her ';
+		} 
+		else {
+			modalSpeciesPatronusTablesSr += 'his ';
 		}
 
-		
+		if (potterCharacter.patronus===''){		
+			modalSpeciesPatronusTablesSr += 'patronus animal is unknown.</div>';
+		} 
+		else {
+			modalSpeciesPatronusTablesSr += 'patronus animal is a '+potterCharacter.patronus+'.</div>'
+		} 
+		modalSpeciesPatronusTablesSr = $(modalSpeciesPatronusTablesSr);
+		$(modalSpeciesPatronusTablesDiv).append(modalSpeciesPatronusTablesSr);
+
+		// Wand table creation
 		var modalTableDiv = $('<div class="modal-table-div"></div>')[0];
 		var modalWandTable = $('<table class="wand-table"></table>')[0];
 		$(modalTableDiv).append(modalWandTable); 
@@ -112,6 +140,9 @@ var potterCharacterRepository = (function () {
 		var wandHeaderRow = modalWandTblHead.insertRow(0); 
 		
 		var wandHeaderText = 'Wand Unknown'; 
+
+		// Set up a div for screen readers
+		var wandSrDiv = '<div class="sr-only">'+potterCharacter.name+'\'s ';
 
 		if (potterCharacter.wand.wood > '') {
 			 
@@ -126,6 +157,7 @@ var potterCharacterRepository = (function () {
 			var woodCell = woodRow.insertCell(1); 
 			var woodText = potterCharacter.wand.wood.charAt(0).toUpperCase() + potterCharacter.wand.wood.slice(1);
 			$(woodCell).text(woodText); 
+			wandSrDiv += 'wand is made of '+ woodText;
 			
 
 
@@ -137,9 +169,14 @@ var potterCharacterRepository = (function () {
 			var coreCell = coreRow.insertCell(1); 
 			var coreText = potterCharacter.wand.core.charAt(0).toUpperCase() + potterCharacter.wand.core.slice(1);
 			if (coreText === '') {
-				coreText = 'Core Unknown';
+				coreText = 'Core Unknown'; 
+				wandSrDiv += ', has a unknown core';
 			}
-			$(coreCell).text(coreText); 
+			else {
+				$(coreCell).text(coreText); 
+				wandSrDiv += ', has a core of '+ coreText;
+			}
+
 			
 
 
@@ -152,45 +189,89 @@ var potterCharacterRepository = (function () {
 			var lengthCell = lengthRow.insertCell(1); 
 			var lengthText = potterCharacter.wand.length + ' in'; 
 			if (lengthText === ' in') {
-				lengthText = 'Length Unknown';
+				lengthText = 'Length Unknown'; 
+				wandSrDiv +=', and an unknown length.</div>';
 			} 
-			$(lengthCell).text(lengthText); 	
+			else {
+				$(lengthCell).text(lengthText); 
+				wandSrDiv +=', and a length of '+ lengthText+'.</div>';
+			}		
 
 		} 
-		$(wandHeaderRow).text(wandHeaderText);
+		else {
+			wandSrDiv = wandSrDiv.substring(0, wandSrDiv.length-3) + ' has no wand or it\'s unknown.</div>';
+		}
+		$(wandHeaderRow).text(wandHeaderText); 
 
+		// Screen reader consideration for wand information 
+		wandSrDiv = $(wandSrDiv);
+		$(modalTableDiv).append(wandSrDiv);
 
+		// Create a div for the character's image
+		var modalImage = $('<img class="character-picture" src="'+ potterCharacter.imageUrl+'">'); 
+		var modalSrImage = $('<div class="sr-only">Image of '+potterCharacter.name+'</div>');
+		var modalImageDiv = $('<div class="modal-image-div"></div>'); 
+		$(modalImageDiv).append(modalImage); 
+		$(modalImageDiv).append(modalSrImage);
 
-
-		var modalImage = $('<img class="character-picture" src="'+ potterCharacter.imageUrl+'">')
-		var modalImageDiv = $('<div class="modal-image-div"></div>');
-		$(modalImageDiv).append(modalImage);
-
-		$(modal).append(closeButton); 
-		$(modal).append(modalTitle);
-		$(modalBodyDiv).append(modalImageDiv);
+		// Append the image, patronus table, species table, and wand table to the main modal body div
+		$(modalBodyDiv).append(modalImageDiv); 
 		$(modalBodyDiv).append(modalSpeciesPatronusTablesDiv); 
 		$(modalBodyDiv).append(modalTableDiv); 
-		$(modal).append(modalBodyDiv);
-		
 
-		$($modalContainer).append(modal);
+		// Append the modal body to the modal content div
+		$(modalContent).append(modalBodyDiv); 
 
-		// Display the modal after all content has been set 
-		$($modalContainer).addClass('is-visible');
+		// Create a footer div and append it with a close button 
+		var modalFooter = $('<div class="modal-footer"></div>'); 
+		var modalCloseButton =  $('<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'); 
+		$(modalCloseButton).on('click',moveListRight);
+		$(modalFooter).append(modalCloseButton); 
+		$(modalContent).append(modalFooter); 
+
+		switch (potterCharacter.house) {
+			case 'Gryffindor': 
+				modalHeader[0].style.borderLeft = '1rem solid rgb(127,9,9)';
+				modalHeader[0].style.borderRight = '1rem solid rgb(127,9,9)'; 
+				modalHeader[0].style.background = 'rgba(127,9,9,.2)';
+				modalBodyDiv[0].style.borderLeft = '1rem solid rgb(255,197,0)';
+				modalBodyDiv[0].style.borderRight = '1rem solid rgb(255,197,0)'; 
+				modalBodyDiv[0].style.background = 'rgba(255,197,0,.2)';
+				break; 
+			case 'Slytherin': 
+				modalHeader[0].style.borderLeft = '1rem solid rgb(13,98,23)';
+				modalHeader[0].style.borderRight = '1rem solid rgb(13,98,23)'; 
+				modalHeader[0].style.background = 'rgba(13,98,23,.2)';
+				modalBodyDiv[0].style.borderLeft = '1rem solid rgb(170,170,170)';
+				modalBodyDiv[0].style.borderRight = '1rem solid rgb(170,170,170)'; 
+				modalBodyDiv[0].style.background = 'rgba(170,170,170,.2)';
+				break; 
+			case 'Hufflepuff': 
+				modalHeader[0].style.borderLeft = '1rem solid rgb(238,225,23)';
+				modalHeader[0].style.borderRight = '1rem solid rgb(238,225,23)'; 
+				modalHeader[0].style.background = 'rgba(238,225,23,.2)';
+				modalBodyDiv[0].style.borderLeft = '1rem solid rgb(0,0,0)';
+				modalBodyDiv[0].style.borderRight = '1rem solid rgb(0,0,0)'; 
+				modalBodyDiv[0].style.background = 'rgba(0,0,0,.2)';
+				break;
+			case 'Ravenclaw':
+				modalHeader[0].style.borderLeft = '1rem solid rgb(0,10,144)';
+				modalHeader[0].style.borderRight = '1rem solid rgb(0,10,144)'; 
+				modalHeader[0].style.background = 'rgba(0,10,144,.2)';
+				modalBodyDiv[0].style.borderLeft = '1rem solid rgb(148,107,45)';
+				modalBodyDiv[0].style.borderRight = '1rem solid rgb(148,107,45)'; 
+				modalBodyDiv[0].style.background = 'rgba(148,107,45,.2)';
+				break;
+		}
+
+
+		$('body').append(modal); 
 	}  
-
-
-	function hideModal() { 
-		$('#modal-container').removeClass('is-visible'); 
-		moveListRight();
-	}
 
 	// Add a listener that will close the modal if the user presses the escape button
 	$(window).on('keydown', (e) => {
-		var $modalContainer = $('#modal-container');
-		if (e.key === 'Escape' && $($modalContainer.hasClass('is-visible'))) {
-			hideModal();
+		if (e.key === 'Escape') {
+			moveListRight();
 		}
 	}); 
 
@@ -249,7 +330,7 @@ var potterCharacterRepository = (function () {
 var potterCharacterLoadingMessage = $('<p class="loading-message">Loading...</p>');
 var potterCharacterTitle = $('#title'); 
 $(potterCharacterTitle).append(potterCharacterLoadingMessage); 
-var potterCharacterList = $('.potter-list'); 
+var potterCharacterList = $('.list-group'); 
 
 potterCharacterRepository.loadList().then(()=>{
   // Data loaded successfully
